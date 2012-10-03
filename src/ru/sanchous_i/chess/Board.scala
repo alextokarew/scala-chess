77286d86f9e51f07f5357d2a2182ad1fbecb19ef
a.tokarev <A.Tokarev@qiwi.ru>
2012-10-03 19:04:04 +0400

    Changing implementation of the Board to HashMap

--- src/ru/sanchous_i/chess/Board.scala
+++ src/ru/sanchous_i/chess/Board.scala
@@ -1,14 +1,9 @@
 package ru.sanchous_i.chess
 
-/**
- * Created with IntelliJ IDEA.
- * User: Sanchous_I
- * Date: 22.09.12
- * Time: 22:49
- * To change this template use File | Settings | File Templates.
- */
+import collection.mutable
+
 class Board {
-  val fields = new Array[Array[(Field,Option[Piece])]](8)
+  val fields = new mutable.HashMap[Field,Piece]
 
   def endGame = false
 
@@ -16,31 +11,27 @@ class Board {
     Nil
   }
 
-  private def figure(col:Int, color:Color) = col match {
-    case 0|7 => Rook(color)
-    case 1|6 => Knight(color)
-    case 2|5 => Bishop(color)
-    case 3 => Queen(color)
-    case 4 => King(color)
-  }
 
-  private def initialPiece(col:Int,row:Int) = row match {
-    case 0 => Some(figure(col,White))
-    case 1 => Some(Pawn(White))
-    case 6 => Some(Pawn(Black))
-    case 7 => Some(figure(col,Black))
-    case _ => None
-  }
+  private def initialPiece(col:Char,row:Int) = {
+    def figure(col:Char, color:Color) = col match {
+      case 'a'|'h' => Rook(color)
+      case 'b'|'g' => Knight(color)
+      case 'c'|'f' => Bishop(color)
+      case 'd' => Queen(color)
+      case 'e' => King(color)
+    }
 
-  for (row <- 0 to 7) {
-    fields(row) = new Array[(Field,Option[Piece])](8)
-    for (col <- 0 to 7) {
-      fields(row)(col) = (Field(('a'+col).toChar,row+1),initialPiece(col,row))
+    row match {
+      case 1 => figure(col,White)
+      case 2 => Pawn(White)
+      case 7 => Pawn(Black)
+      case 8 => figure(col,Black)
     }
   }
 
-  override def toString =
-    (for (col <- fields)
-      yield (for (field <- col)
-        yield field._1.toString()+field._2.toString()).reduceLeft(_ + " " + _)).reduceLeft(_ + "\r\n" + _)
+  for (row <- List(1,2,7,8))
+    for (col <- Field.cols)
+      fields put (Field(col,row),initialPiece(col,row))
+
+  override def toString = fields.toString()
 }
